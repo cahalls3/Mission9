87b593f05622044819da6858556b1330dac0e35b
cahalls3 <carsonahalls3@gmail.com>
2022-03-08 21:50:32 -0700

    More copying

---
+++ Mission9/Pages/PurchaseCompleted.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mission9.Infrastructure;
+using Mission9.Models;
+
+namespace Mission9.Pages
+{
+    public class PurchaseCompleted : PageModel
+    {
+        public PurchaseCompleted()
+        {
+        }
+    }
+}
