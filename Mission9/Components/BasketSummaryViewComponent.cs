b06905f544e1fc555ddfb56f50d2e8c472288484
cahalls3 <carsonahalls3@gmail.com>
2022-03-10 10:22:24 -0700

    Cart icon

---
+++ Mission9/Components/BasketSummaryViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission9.Models;
+namespace SportsStore.Components
+{
+    public class BasketSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public BasketSummaryViewComponent(Basket basketService)
+        {
+            basket = basketService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
